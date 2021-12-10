f7aceb007e57b02f7b923c75f3cf7aed9d6dca00
potatozhb <zhaohongbo1980@gmail.com>
2021-12-09 21:04:47 -0600

    Add project files.

---
+++ ASPDotNetSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPDotNetSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
